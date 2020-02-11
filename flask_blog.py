d5f20144c3991ede888387946d60559b4bca2189
Boadzie Daniel <boadziedaniel43@gmail.com>
2020-02-11 16:50:03 +0000

    added register and login forms, routes, views and validated them

--- flask_blog.py
+++ flask_blog.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'd2c0a709d897c6e434c34ad0a93ea5b8'
 
 
 posts = [
@@ -26,6 +29,27 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+# register
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+# login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 
